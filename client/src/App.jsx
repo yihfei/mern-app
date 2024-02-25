cce4807a54158bc4ed60e7e10aa98ee06739478b
yihfei <lim.yih.fei@gmail.com>
2024-02-25 18:15:14 +0800

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Brews from './pages/Brews'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/> 
+        <Route path="/about" element={<About />}/> 
+        <Route path="/brews" element={<Brews/>}/> 
+        <Route path="/sign-in" element={<SignIn/>}/> 
+        <Route path="/sign-up" element={<SignUp />}/> 
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Brews.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Brews() {
+  return (
+    <div>Brews</div>
+  )
+}
