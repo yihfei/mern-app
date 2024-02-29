960da8b58da90dbc5f16a20132bbca4b24d8c62a
yihfei <lim.yih.fei@gmail.com>
2024-02-29 18:04:45 +0800

    Complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,11 @@
 import { React, useEffect, useRef, useState } from 'react'
 import { Alert, Button, TextInput } from 'flowbite-react';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch  } from 'react-redux'
+import {
+    updateStart,
+    updateSuccess,
+    updateFailure,
+  } from '../redux/user/userSlice';
 import {
     getDownloadURL,
     getStorage,
@@ -15,8 +20,13 @@ export default function Profile() {
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
-    
+    const [imageFileUploading, setImageFileUploading] = useState(false);
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const [updateUserError, setUpdateUserError] = useState(null);
+    const [formData, setFormData] = useState({});
+    const dispatch = useDispatch();
     const filePickerRef = useRef();
+
     const handleImageChange = (e) => {
         const file = e.target.files[0]
         if (file) {
@@ -42,6 +52,7 @@ export default function Profile() {
         //     }
         //   }
         // }
+        setImageFileUploading(true);
         setImageFileUploadError(null);
         const storage = getStorage(app);
         const fileName = new Date().getTime() + imageFile.name;
@@ -62,20 +73,63 @@ export default function Profile() {
             setImageFileUploadProgress(null);
             setImageFile(null);
             setImageFileUrl(null);
+            setImageFileUploading(false);
           },
           () => {
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
               setImageFileUrl(downloadURL);
+              setFormData({ ...formData, profilePicture: downloadURL });
+              setImageFileUploading(false);
             });
           }
         );
       };
+
+      const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        setUpdateUserError(null);
+        setUpdateUserSuccess(null);
+        if (Object.keys(formData).length === 0) {
+          setUpdateUserError('No changes made');
+          return;
+        }
+        if (imageFileUploading) {
+          setUpdateUserError('Please wait for image to upload');
+          return;
+        }
+        try {
+          dispatch(updateStart());
+          const res = await fetch(`/api/user/update/${currentUser._id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            dispatch(updateFailure(data.message));
+            setUpdateUserError(data.message);
+          } else {
+            dispatch(updateSuccess(data));
+            setUpdateUserSuccess("User's profile updated successfully");
+          }
+        } catch (error) {
+          dispatch(updateFailure(error.message));
+          setUpdateUserError(error.message);
+        }
+      };
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>
             Profile
         </h1>
-        <form className='flex flex-col gap-4'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
             <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
             <div className='w-32 h-32 self-center cursor-pointer shadow-md
             overflow-hidden rounded-full' onClick={()=> filePickerRef.current.click()}>
@@ -86,9 +140,9 @@ export default function Profile() {
             {imageFileUploadError && (
                 <Alert color='failure'>{imageFileUploadError}</Alert>
             )}
-            <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
-            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} />
-            <TextInput type='password' id='passwprd' placeholder='password' />
+            <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange}/>
+            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
+            <TextInput type='password' id='passwprd' placeholder='password' onChange={handleChange}/>
             <Button type='submit' outline>
                 Update
             </Button>
@@ -99,6 +153,16 @@ export default function Profile() {
             <span className='cursor-pointer'>Sign Out</span>
 
         </div>
+        {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+          {updateUserSuccess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert color='failure' className='mt-5'>
+          {updateUserError}
+        </Alert>
+      )}
         
 
     </div>
