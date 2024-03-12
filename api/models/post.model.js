7930c3e48c89c04f714ee758351804fc7fcdd941
yihfei <lim.yih.fei@gmail.com>
2024-03-12 21:31:50 +0800

    Complete create brew

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -28,11 +28,7 @@ const postSchema = new mongoose.Schema({
       type: String,
       required: true
     },
-    image: {
-      type: String,
-      default:
-        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
-    },
+    
   });
   
 const Post = mongoose.model('Post', postSchema);
--- client/src/pages/CreateBrew.jsx
+++ client/src/pages/CreateBrew.jsx
@@ -1,21 +1,74 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react';
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function CreateBrew() {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log("reached")
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
+    }
+  };
+
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Record a Coffee Brew</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <TextInput
           type='text'
           placeholder='Coffee Name'
           required
           id='coffeeName'
+          onChange={(e) =>
+            setFormData({ ...formData, coffeeName: e.target.value })
+          }
         />
         <div className='flex gap-4'>
-          <Select>
-            <option value='select'>Select a Brewing Method</option>
+          <Select
+            onChange={(e) =>
+              setFormData({ ...formData, brewingMethod: e.target.value })
+            }
+          >
+            <option value='uncategorized'>Select a Brewing Method</option>
             <option value='aeropress'>Aeropress</option>
             <option value='chemex'>Chemex</option>
             <option value='frenchPress'>French Press</option>
@@ -24,15 +77,20 @@ export default function CreateBrew() {
           <TextInput
             type='number'
             placeholder='Brew Time (minutes)'
-            required
             id='brewTime'
             className='flex-1'
+            onChange={(e) =>
+              setFormData({ ...formData, brewTime: e.target.value })
+            }
           />
           <TextInput
             type='text'
             placeholder='Grind Size'
             id='grindSize'
             className = 'w-24'
+            onChange={(e) =>
+              setFormData({ ...formData, grindSize: e.target.value })
+            }
         />
         </div>
 
@@ -43,12 +101,18 @@ export default function CreateBrew() {
             id='waterTemperature'
             step='0.1' // Adjust step size as needed
             className='flex-1' // Make the input field take up remaining space
+            onChange={(e) =>
+              setFormData({ ...formData, waterTemperature: e.target.value })
+            }
           />
           <TextInput
             type='text'
             placeholder='Coffee to Water Ratio (e.g., 1:15)'
             id='coffeeWaterRatio'
             className='flex-1' // Make the input field take up remaining space
+            onChange={(e) =>
+              setFormData({ ...formData, coffeeWaterRatio: e.target.value })
+            }
           />
         </div>
         <ReactQuill
@@ -56,22 +120,16 @@ export default function CreateBrew() {
           placeholder='notes...'
           className='h-72 mb-12'
           required
+          onChange={(value) => {
+            setFormData({ ...formData, notes: value });
+          }}
         />
-        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
-          <FileInput type='file' accept='image/*' />
-          <Button
-            type='button'
-            gradientDuoTone='purpleToBlue'
-            size='sm'
-            outline
-          >
-            Upload image
-          </Button>
-        </div>
-        <Button type='submit' gradientDuoTone='greenToYellow'>
+        
+        <Button type='submit' >
           Record Brew
         </Button>
       </form>
     </div>
   );
 }
+
